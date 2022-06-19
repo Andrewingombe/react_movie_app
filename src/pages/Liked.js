574d0d9f16183017689d73e4797e35a291d8719b
Andrew Ingombe <ingombe.ani@gmail.com>
2022-06-19 19:23:39 +0200

    fix: fix ui elements on the pages to make it look nice.

--- src/pages/Liked.js
+++ src/pages/Liked.js
@@ -6,11 +6,21 @@ const Liked = () => {
 
   return (
     <div>
-      <h2 className="page-title">Liked Movies</h2>
-      <div className="home">
-        {likedMovies &&
-          likedMovies.map((movie) => <Movie key={movie.id} movie={movie} />)}
-      </div>
+      {likedMovies.length === 0 ? (
+        <div className="no-liked-movies">
+          <h2>There are no liked movies...</h2>
+        </div>
+      ) : (
+        <>
+          <h2 className="page-title">Liked Movies</h2>
+          <div className="home">
+            {likedMovies &&
+              likedMovies.map((movie) => (
+                <Movie key={movie.id} movie={movie} />
+              ))}
+          </div>
+        </>
+      )}
     </div>
   );
 };
